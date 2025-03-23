06078b77a1205aea43c60b39a69b27476227d0f8
Grant Maskus <gemaskus@gmail.com>
2025-03-23 16:14:53 -0600

    changed how things where read in, now line by line combined 8 bytes at a time

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,13 +15,22 @@ func main() {
 	if er != nil {
 		log.Fatalf("Could not open the file: %s", fileName)
 	}
+
+	var line string
 	buffer := make([]byte, 8)
 	for {
 		n, err := file.Read(buffer)
 
 		// Print read in byte string
 		if n > 0 {
-			fmt.Printf("read: %s\n", buffer[:n])
+			parts := strings.Split(string(buffer[:n]), "\n")
+
+			for i := 0; i < len(parts)-1; i++ {
+				fmt.Printf("read: %s%s\n", line, parts[i])
+				line = ""
+			}
+
+			line += parts[len(parts)-1]
 		}
 
 		if err == io.EOF {
