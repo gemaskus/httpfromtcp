a7cef8b1d53b0f3fe4528ed6a1d3c53a2441e160
Grant Maskus <gemaskus@gmail.com>
2025-03-31 14:58:44 -0600

    httpfromtcp
    boot.dev http course: ch1 lesson 4
    converted the reading to use a channel and run in a concurrent go
    routine

--- main.go
+++ main.go
@@ -16,29 +16,74 @@ func main() {
 		log.Fatalf("Could not open the file: %s", fileName)
 	}
 
-	var line string
-	buffer := make([]byte, 8)
-	for {
-		n, err := file.Read(buffer)
+	lines := getLinesChannel(file)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
+	}
 
-		// Print read in byte string
-		if n > 0 {
-			parts := strings.Split(string(buffer[:n]), "\n")
+	// var line string
+	// buffer := make([]byte, 8)
+	//
+	//	for {
+	//		n, err := file.Read(buffer)
+	//
+	//		// Print read in byte string
+	//		if n > 0 {
+	//			parts := strings.Split(string(buffer[:n]), "\n")
+	//
+	//			for i := 0; i < len(parts)-1; i++ {
+	//				fmt.Printf("read: %s%s\n", line, parts[i])
+	//				line = ""
+	//			}
+	//
+	//			line += parts[len(parts)-1]
+	//		}
+	//
+	//		if err == io.EOF {
+	//			break //we hit the end of the file
+	//		}
+	//
+	//		if err != nil {
+	//			log.Fatalf("Failed to read from the file: %v", err)
+	//		}
+	//	}
+}
 
-			for i := 0; i < len(parts)-1; i++ {
-				fmt.Printf("read: %s%s\n", line, parts[i])
-				line = ""
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	fileChannel := make(chan string)
+
+	go func() {
+		var line string
+		buffer := make([]byte, 8)
+		for {
+			n, err := f.Read(buffer)
+
+			// Print read in byte string
+			if n > 0 {
+				parts := strings.Split(string(buffer[:n]), "\n")
+
+				for i := 0; i < len(parts)-1; i++ {
+					fileChannel <- line + parts[i]
+					line = ""
+				}
+
+				line += parts[len(parts)-1]
 			}
 
-			line += parts[len(parts)-1]
-		}
+			if err == io.EOF {
+				if line != "" {
+					fileChannel <- line
+				}
+				break //we hit the end of the file
+			}
 
-		if err == io.EOF {
-			break //we hit the end of the file
+			if err != nil {
+				log.Fatalf("Failed to read from the file: %v", err)
+			}
 		}
+		f.Close()
+		close(fileChannel)
+	}()
 
-		if err != nil {
-			log.Fatalf("Failed to read from the file: %v", err)
-		}
-	}
+	return fileChannel
 }
