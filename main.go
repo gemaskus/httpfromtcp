c4c1af0a5bf9dec79f1ed9eb725e8bdbcbad3664
Grant Maskus <gemaskus@gmail.com>
2025-04-01 14:17:06 -0600

    boot.dev httpfromtcp course - setup a tcp listener on a port - route the traffic from that port to the channel from before - everything works!

--- main.go
+++ main.go
@@ -4,49 +4,34 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
+	//	"os"
 	"strings"
 )
 
 func main() {
-	var fileName string = "messages.txt"
+	//	var fileName string = "messages.txt"
 
-	file, er := os.Open(fileName)
-	if er != nil {
-		log.Fatalf("Could not open the file: %s", fileName)
+	listener, err := net.Listen("tcp", ":42069")
+	if err != nil {
+		log.Fatalf("Could not create TCP listener: %v", err)
 	}
 
-	lines := getLinesChannel(file)
-	for line := range lines {
-		fmt.Printf("read: %s\n", line)
+	for {
+		conn, err := listener.Accept()
+
+		lines := getLinesChannel(conn)
+		for line := range lines {
+			fmt.Printf("%s\n", line)
+		}
+		if err != nil {
+			listener.Close()
+			log.Fatalf("Could not accept the connection: %v\n", err)
+		}
+
 	}
 
-	// var line string
-	// buffer := make([]byte, 8)
-	//
-	//	for {
-	//		n, err := file.Read(buffer)
-	//
-	//		// Print read in byte string
-	//		if n > 0 {
-	//			parts := strings.Split(string(buffer[:n]), "\n")
-	//
-	//			for i := 0; i < len(parts)-1; i++ {
-	//				fmt.Printf("read: %s%s\n", line, parts[i])
-	//				line = ""
-	//			}
-	//
-	//			line += parts[len(parts)-1]
-	//		}
-	//
-	//		if err == io.EOF {
-	//			break //we hit the end of the file
-	//		}
-	//
-	//		if err != nil {
-	//			log.Fatalf("Failed to read from the file: %v", err)
-	//		}
-	//	}
+	listener.Close()
 }
 
 func getLinesChannel(f io.ReadCloser) <-chan string {
@@ -83,6 +68,7 @@ func getLinesChannel(f io.ReadCloser) <-chan string {
 		}
 		f.Close()
 		close(fileChannel)
+		fmt.Printf("Channel is closed\n")
 	}()
 
 	return fileChannel
